792bd5fbb5b3a45e329aa910b87dc8d8851516f2
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-06-26 15:37:19 +0700

    8. Hiển thị danh sách chuyên môn trong ManGiangVienChuyenMon

--- app/src/main/java/com/example/tranhuubinh17dcat023/ManGiangVienChuyenMon.java
+++ app/src/main/java/com/example/tranhuubinh17dcat023/ManGiangVienChuyenMon.java
@@ -2,13 +2,51 @@ package com.example.tranhuubinh17dcat023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.tranhuubinh17dcat023.adapter.adapterChuyenMon;
+import com.example.tranhuubinh17dcat023.database.database;
+import com.example.tranhuubinh17dcat023.model.B17DCAT023_ChuyenMon;
+
+import java.util.ArrayList;
 
 public class ManGiangVienChuyenMon extends AppCompatActivity {
+
+    ListView lvListChuyenMon;
+    Button btnAddChuyenMonGiangVien;
+
+    ArrayList<B17DCAT023_ChuyenMon> chuyenMonArrayList;
+
+    adapterChuyenMon adapterChuyenMon;
+
+    database databaseQLGV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_giang_vien_chuyen_mon);
+
+        databaseQLGV = new database(this);
+
+        // Ánh xạ
+        lvListChuyenMon = findViewById(R.id.lvListChuyenMon);
+        btnAddChuyenMonGiangVien = findViewById(R.id.btnAddChuyenMonGiangVien);
+
+        btnAddChuyenMonGiangVien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManGiangVienChuyenMon.this, ManQuanLy.class);
+                startActivity(intent);
+            }
+        });
+
+        chuyenMonArrayList = databaseQLGV.getAllChuyenMon();
+        adapterChuyenMon = new adapterChuyenMon(getApplicationContext(), chuyenMonArrayList);
+        lvListChuyenMon.setAdapter(adapterChuyenMon);
     }
 }
---
+++ app/src/main/java/com/example/tranhuubinh17dcat023/ManQuanLy.java
@@ -0,0 +1,14 @@
+package com.example.tranhuubinh17dcat023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManQuanLy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_quan_ly);
+    }
+}
