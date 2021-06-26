377a883fa4885c1f8fb6f151c30f9e77b6e66bc5
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-06-26 14:31:02 +0700

    2. Thiết kế giao diện trang chủ

--- app/src/main/java/com/example/tranhuubinh17dcat023/MainActivity.java
+++ app/src/main/java/com/example/tranhuubinh17dcat023/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.tranhuubinh17dcat023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTimKiem, btnGiangVien, btnChuyenMon, btnQuanLy, btnThongKe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTimKiem = findViewById(R.id.btnTimKiem);
+        btnGiangVien = findViewById(R.id.btnGiangVien);
+        btnChuyenMon = findViewById(R.id.btnChuyenMon);
+        btnQuanLy = findViewById(R.id.btnQuanLy);
+        btnThongKe = findViewById(R.id.btnThongKe);
+
+        btnTimKiem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManTimKiem.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGiangVien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManGiangVien.class);
+                startActivity(intent);
+            }
+        });
+
+        btnChuyenMon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManChuyenMon.class);
+                startActivity(intent);
+            }
+        });
+
+        btnQuanLy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManGiangVienChuyenMon.class);
+                startActivity(intent);
+            }
+        });
+
+        btnThongKe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ManThongKe.class);
+                startActivity(intent);
+            }
+        });
     }
 }
